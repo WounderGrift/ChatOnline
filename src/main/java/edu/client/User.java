66ab0fe3df31aadd3789de27fedc847a42598a18
WounderGrift <woundergrift@gmail.com>
2019-07-18 15:06:33 +0400

    New object User

---
+++ src/main/java/edu/client/User.java
@@ -0,0 +1,30 @@
+package edu.client;
+
+/**
+ * Created by Dima on 18.07.2019.
+ */
+public class User {
+    private String name;
+    private String pass;
+
+    public User(String name, String pass) {
+        this.name = name;
+        this.pass = pass;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    void setPass(String pass) {
+        this.pass = pass;
+    }
+}
